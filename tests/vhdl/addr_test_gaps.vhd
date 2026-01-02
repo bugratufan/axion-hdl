3689d32a716dd3871a18b70c0f8de6466b647309
Buğra Tufan <btufan21@ku.edu.tr>
2026-01-02 18:12:37 +0300

    fix(tests): update addr_test_gaps.vhd annotation syntax

--- tests/vhdl/addr_test_gaps.vhd
+++ tests/vhdl/addr_test_gaps.vhd
@@ -4,21 +4,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- @axion_def BASE_ADDR=0x0000
+
 entity addr_test_gaps is
     port (
         clk : in std_logic;
-        -- @axion BASE_ADDR=0x0000
-        -- @axion RW ADDR=0x00
-        gap_reg_a : inout std_logic_vector(31 downto 0);
-        -- @axion RW ADDR=0x10
-        gap_reg_b : inout std_logic_vector(31 downto 0);
-        -- @axion RW ADDR=0x20
-        gap_reg_c : inout std_logic_vector(31 downto 0);
-        -- @axion RW ADDR=0x24
-        gap_reg_d : inout std_logic_vector(31 downto 0)
+        rst_n : in std_logic
     );
 end entity addr_test_gaps;
 
 architecture rtl of addr_test_gaps is
+    signal gap_reg_a : std_logic_vector(31 downto 0); -- @axion: RW ADDR=0x00
+    signal gap_reg_b : std_logic_vector(31 downto 0); -- @axion: RW ADDR=0x10
+    signal gap_reg_c : std_logic_vector(31 downto 0); -- @axion: RW ADDR=0x20
+    signal gap_reg_d : std_logic_vector(31 downto 0); -- @axion: RW ADDR=0x24
 begin
 end architecture rtl;
