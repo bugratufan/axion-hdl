c248a9b64bae4890ee597d2ab1a664f2b2717a14
Buğra Tufan <btufan21@ku.edu.tr>
2026-04-11 13:05:17 +0300

    Revert "fix: handle None default_value from SV parser in generator and doc outputs"
    This reverts commit bd25d36f002dee84fcacfaff70859a0aa8b25ca3.

--- axion_hdl/generator.py
+++ axion_hdl/generator.py
@@ -758,7 +758,7 @@ class VHDLGenerator:
                 continue
             if reg['access_mode'] in ['WO', 'RW']:
                 num_regs = self._get_num_regs(reg['signal_type'])
-                default_val = reg.get('default_value') or 0
+                default_val = reg.get('default_value', 0)
                 if num_regs == 1:
                     if default_val != 0:
                         lines.append(f"                {reg['signal_name']}_reg <= x\"{default_val:08X}\";")
